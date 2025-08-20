27e610fd5c7c8bf263ffe75182216ca6cbb03c71
abubakarraja345-source <abubakarraja345@gmail.com>
2025-08-20 11:14:05 +0500

    four

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const searchInput = document.getElementById("searchInput");
 const themeToggle = document.getElementById("themeToggle");
 
 async function fetchNews() {
-  const url = `/api/news`; 
+const url = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}`;
   loadNews(url);
 }
 
